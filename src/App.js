ac2c7fb651de03cc10885f20d5c2a6dbeb6eb8b0
sara-R98 <sarah.rahgoshay@gmail.com>
2022-12-01 09:58:58 +0330

    feat: add initial components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+//Componenets
+import Store from './components/Store';
+//route
+import { Navigate, Route, Routes} from 'react-router-dom';
+import ProductDetails from './components/ProductDetails';
+import Navbar from './components/shared/Navbar';
+import ShopCart from './components/ShopCart';
+//Redux
+import { Provider } from 'react-redux';
+import store from "./redux/store"
+import ProductsDetails from './components/ProductsDetails';
+
+const App = () => {
+  return (
+    <div>
+      <Provider store= {store}>
+        <Navbar />
+          <Routes>
+            <Route path='/products/:id' element={<ProductsDetails />} />
+            <Route path='/products' element= {<Store />} />
+            <Route path='/cart' element={<ShopCart/>} />
+            <Route path='/*' element={<Navigate to='/products' />} />
+          </Routes>
+      </Provider>
+          
+    </div>
+  );
+};
+
+export default App;
